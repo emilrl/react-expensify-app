9c3fa6b877b4b1053f1d606ebee2223e4da0cb29
Emil Larsen <emil1708@gmail.com>
2020-08-02 11:14:27 +0900

    store user data in user area

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { startLogout } from '../actions/auth';
-import { connect } from 'react-redux';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { startLogout } from "../actions/auth";
+import { connect } from "react-redux";
 
 export const Header = ({ startLogout }) => (
-	<header>
-		<h1>Expensify</h1>
-		<NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-		<NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-		<button onClick={startLogout}>Logout</button>	
-	</header>
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/dashboard" activeClassName="is-active">
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
+    <button onClick={startLogout}>Logout</button>
+  </header>
 );
 
-
 const mapDispatchToProps = (dispatch) => ({
-	startLogout: () => dispatch(startLogout())
+  startLogout: () => dispatch(startLogout()),
 });
 
 export default connect(undefined, mapDispatchToProps)(Header);
