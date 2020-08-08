cf3cd4b5537e58bfeaee273c976331dd9eee804c
Emil Larsen <emil1708@gmail.com>
2020-08-08 09:02:41 +0900

    Add Expensify styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,66 +1,88 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/fiters';
-import { DateRangePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
+import React from "react";
+import { connect } from "react-redux";
+import {
+  setTextFilter,
+  sortByDate,
+  sortByAmount,
+  setStartDate,
+  setEndDate,
+} from "../actions/fiters";
+import { DateRangePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
 
 export class ExpenseListFilters extends React.Component {
-	state = {
-		calendarFocused: null,
-	}
-	onFocusChange = (calendarFocused) => {
-		this.setState(() => ({ calendarFocused }))
-	};
-	onDatesChange = ({ startDate, endDate }) => {
-		this.props.setStartDate(startDate);
-		this.props.setEndDate(endDate);
-	};
-	onTextChange = (e) => {
-		this.props.setTextFilter(e.target.value)
-	}
-	onSortChange = (e) => {
-		if (e.target.value==="date") {
-			this.props.sortByDate()
-		} else if (e.target.value==="amount") {
-			this.props.sortByAmount()
-		}
-	}
-	render() {
-		return (
-			<div>
-				<input type='text' value={this.props.filters.text} onChange={this.onTextChange}/>
-				<select 
-					value={this.props.filters.sortBy}
-					onChange={this.onSortChange}
-				>
-					<option value="date">Date</option>
-					<option value="amount">Amount</option>
-				</select>
-				<DateRangePicker 
-					startDate={this.props.filters.startDate}
-					endDate={this.props.filters.endDate}
-					focusedInput={this.state.calendarFocused}
-					onFocusChange={this.onFocusChange}
-					onDatesChange={this.onDatesChange}
-					numberOfMonths={1}
-					isOutsideRange={(day) => false}
-					showClearDates={true}
-				/>
-			</div>
-		)
-	};
-};
+  state = {
+    calendarFocused: null,
+  };
+  onFocusChange = (calendarFocused) => {
+    this.setState(() => ({ calendarFocused }));
+  };
+  onDatesChange = ({ startDate, endDate }) => {
+    this.props.setStartDate(startDate);
+    this.props.setEndDate(endDate);
+  };
+  onTextChange = (e) => {
+    this.props.setTextFilter(e.target.value);
+  };
+  onSortChange = (e) => {
+    if (e.target.value === "date") {
+      this.props.sortByDate();
+    } else if (e.target.value === "amount") {
+      this.props.sortByAmount();
+    }
+  };
+  render() {
+    return (
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">
+            <input
+              type="text"
+              className="text-input"
+              placeholder="Search expenses"
+              value={this.props.filters.text}
+              onChange={this.onTextChange}
+            />
+          </div>
+          <div className="input-group__item">
+            <select
+              className="select"
+              value={this.props.filters.sortBy}
+              onChange={this.onSortChange}
+            >
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker
+              startDate={this.props.filters.startDate}
+              endDate={this.props.filters.endDate}
+              focusedInput={this.state.calendarFocused}
+              onFocusChange={this.onFocusChange}
+              onDatesChange={this.onDatesChange}
+              numberOfMonths={1}
+              isOutsideRange={(day) => false}
+              showClearDates={true}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
-	filters: state.filters
+  filters: state.filters,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	setStartDate: (startDate) => dispatch(setStartDate(startDate)),
-	setEndDate: (endDate) => dispatch(setEndDate(endDate)), 
-	setTextFilter: (text) => dispatch(setTextFilter(text)),
-	sortByDate: () => dispatch(sortByDate()),
-	sortByAmount: () => dispatch(sortByAmount())
+  setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+  setEndDate: (endDate) => dispatch(setEndDate(endDate)),
+  setTextFilter: (text) => dispatch(setTextFilter(text)),
+  sortByDate: () => dispatch(sortByDate()),
+  sortByAmount: () => dispatch(sortByAmount()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,86 @@
 // import DotEnv from 'dotenv';
-const path = require('path');
-const webpack = require('webpack');
-const ExtraTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtraTextPlugin = require("extract-text-webpack-plugin");
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-if (process.env.NODE_ENV === 'test') {
-	require('dotenv').config({ path: '.env.test' });
-	// DotEnv.config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
-	require('dotenv').config({ path: '.env.development' });
-	// DotEnv.config({ path: '.env.development' });
-};
+if (process.env.NODE_ENV === "test") {
+  require("dotenv").config({ path: ".env.test" });
+  // DotEnv.config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+  // DotEnv.config({ path: '.env.development' });
+}
 
 // process.env.NODE_ENV
 
 module.exports = (env) => {
-	const isProduction = env === 'production';
-	const CSSExtract = new ExtraTextPlugin('styles.css')
+  const isProduction = env === "production";
+  const CSSExtract = new ExtraTextPlugin("styles.css");
 
-	return {
-		entry: './src/app.js',
-		output: {
-			filename: 'bundle.js',
-			path: path.resolve(__dirname, 'public', 'dist')
-		},
-		module: {
-			rules: [{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-
-			}, {
-				test: /\.s?css$/,
-				use: CSSExtract.extract({
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						}
-					]
-				})
-			}]
-		},
-		plugins: [
-			CSSExtract,
-			new webpack.DefinePlugin({
-				'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-				'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-				'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-				'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-				'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-				'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-			})
-		],
-		devtool: isProduction ? 'source-map' : 'inline-source-map',
-		devServer: {
-			contentBase: path.join(__dirname, 'public'),
-			historyApiFallback: true,
-			publicPath: '/dist/'
-		}
-	};
+  return {
+    entry: ["babel-polyfill", "./src/app.js"],
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "public", "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
+    },
+    plugins: [
+      CSSExtract,
+      new webpack.DefinePlugin({
+        "process.env.FIREBASE_API_KEY": JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
+        ),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+      }),
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/",
+    },
+  };
 };
